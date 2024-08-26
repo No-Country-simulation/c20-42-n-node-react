607f7baebfd143b50e22cb8236d2c79295cdb287
carlos alecrim <csilva1073@gmail.com>
2024-08-26 16:34:48 -0400

    fix(model): modificacion - nombre del model

--- Backend/api/migrations/20240824221543-create-mascota.js
+++ Backend/api/migrations/20240824221543-create-mascota.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Mascota', {
+    await queryInterface.createTable('Mascotas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -53,6 +53,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Mascota');
+    await queryInterface.dropTable('Mascotas');
   }
 };
