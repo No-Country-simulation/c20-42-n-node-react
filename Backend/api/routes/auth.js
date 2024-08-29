de7aa9827bc2d75f1c3b55c5403604b0f2af1af4
carlos alecrim <csilva1073@gmail.com>
2024-08-29 16:59:21 -0400

    eliminación de código

--- Backend/api/routes/auth.js
+++ Backend/api/routes/auth.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
-// const passport = require('../middleware/passport');
-// const {Usuario} = require('../models/usuario');
-// const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authController = require('../controllers/auth.controller');
 
