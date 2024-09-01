f00fd1f04b94046a782575c14bf31bebc70225da
carlos alecrim <csilva1073@gmail.com>
2024-09-01 15:57:21 -0400

    eliminacion de líneas que no se usan

--- Backend/api/routes/centroadopcion.js
+++ Backend/api/routes/centroadopcion.js
@@ -1,7 +1,12 @@
 var express = require('express');
 const centroAdopcionController = require('../controllers/centroadopcion.controller');
 var router = express.Router();
-
+/**
+ * @swagger
+ * /centroadopcion/list:
+ * get:
+ *  sumary: lista de centros de adopcion
+ */ 
 router.get('/list', centroAdopcionController.getAllCentroAdopcion);
 
 module.exports = router;
