cf4e8703bcf6160c973f5135c3de2f67386aee94
carlos alecrim <csilva1073@gmail.com>
2024-09-01 15:52:38 -0400

    archivo de configuracion swagger

---
+++ Backend/api/config/swagger.js
@@ -0,0 +1,24 @@
+const swaggerAutogen = require('swagger-autogen')({openapi:'3.0.0'});
+
+const doc = {
+    info:{
+        title:'Adopcion Api',
+        description: 'Api para gestinar los centros de adopcion'
+    },
+    host:'localhost:3000',
+    schemes:['http','https'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+};
+
+const outputFile = '../swagger-output.json';
+const endpointsFiles = [
+    '../routes/centroadopcion.js',
+    '../app.js'
+]
+
+swaggerAutogen(outputFile,endpointsFiles,doc).then(
+    ()=>{
+        require('../app')
+    }
+);
