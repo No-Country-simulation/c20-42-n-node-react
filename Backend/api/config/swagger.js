50e204d0e3ade2556b65d7d3c7f3189ca295ba34
carlos alecrim <csilva1073@gmail.com>
2024-09-05 14:46:05 -0400

    añadido configuracion para uso de jwt en la documentacion de Api

--- Backend/api/config/swagger.js
+++ Backend/api/config/swagger.js
@@ -5,6 +5,18 @@ const doc = {
         title:'Adopcion Api',
         description: 'Api para gestinar los centros de adopcion'
     },
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+            }
+        }
+    },
+    security: [{
+        bearerAuth: []
+    }],
     host:'localhost:3000',
     schemes:['http','https'],
     consumes: ['application/json'],
