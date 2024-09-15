942d641726240fb947d18646d816b422c9fdaffe
carlos alecrim <csilva1073@gmail.com>
2024-09-15 17:10:21 -0400

    creacion de menu lateral para gerenciar mascota

--- Frontend/Adopet/src/pages/Mascota/MascotaPage.jsx
+++ Frontend/Adopet/src/pages/Mascota/MascotaPage.jsx
@@ -1,13 +1,29 @@
-import FormMascota from "../../components/Mascota/FormMascota";
-
+import { Link, Outlet } from "react-router-dom";
 
 const MascotaPage =()=>{
     return(
         <div className="container">
-            <h2>mascota</h2>
+            <div className="row">
+                <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+                <div className="position-sticky">
+                    <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/mascotas/create">Crear</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/mascotas/">Listar</Link>
+                    </li>
+                    </ul>
+                </div>
+                </nav>
 
-            <FormMascota/>
-        </div>
+                <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <h2>Mascotas</h2>
+                <br/>
+                    <Outlet /> 
+                </main>
+            </div>
+            </div>
         
     );
 }
