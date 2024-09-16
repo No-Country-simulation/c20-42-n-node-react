e48137475195ba15b99100dfda51220a7e859ea5
carlos alecrim <csilva1073@gmail.com>
2024-09-16 19:30:24 -0400

    fix: modificacion de archivo de variaveis de entorno

--- Frontend/Adopet/vite.config.js
+++ Frontend/Adopet/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) =>{
+  const env = loadEnv(mode,process.cwd());
+
+  const API_URL = `${env.VITE_BACKEND_URL}`;
+  return {
   plugins: [react()],
   server:{
     proxy:{
       '/api':{
-        target: process.env.VITE_BACKEND_URL,
+        target: API_URL,
         changeOrigin:true,
         secure:false
       }
     }
   }
-})
+};
+});
+
